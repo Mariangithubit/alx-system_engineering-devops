2ff6ced501d081355131e4c7eda233500a9d8b31
Mariangithubit <metsyb1@hotmail.com>
2023-09-23 15:33:28 -0700

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+#Setting Client configuration file (w/ Puppet)
+include stdlib
+
+file_line { 'Turn off passowrd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'True identify file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentifyAuthentication ~/.ssh/school',
+  replace => true,
+}
