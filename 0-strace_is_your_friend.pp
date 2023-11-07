92edd615833e7534473145fe92aa0186aa06a087
Mariangithubit <metsyb1@hotmail.com>
2023-11-07 13:39:18 -0800

    Strace is your friend

---
+++ 0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixe bad "phpp" extensions to "php" in the WordPress file.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
